0398ae189990f9d173ab1eacedb4440dfe1a8e72
V11an <allankertich@gmail.com>
2024-05-07 22:27:04 +0000

    correctins

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """Return total number of subscribers on given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "vllan.agent:v1.0.0 (by /u/vllan)"
+            "User-Agent": "vllan:agent:v1.0.0 (by /u/vllan)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
