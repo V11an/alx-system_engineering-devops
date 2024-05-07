cca67ecddb5d0cca67e607bddb68353ae9b559ab
V11an <allankertich@gmail.com>
2024-05-08 00:54:48 +0300

    querry api to return number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Function used to query subscribers on given Reddit subreddit."""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return total number of subscribers on given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "vllan.agent:v1.0.0 (by /u/vllan)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
